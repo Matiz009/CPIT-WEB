e401d3e1b9c8a2f8381b6bd887228b6bf5b60795
Mati ul Rehman <mati.rehman.dev@gmail.com>
2026-02-25 20:28:14 +0500

    js deep dive

--- bootstrap/script.js
+++ bootstrap/script.js
@@ -1,5 +1,20 @@
-let h1Element = document.getElementById("h1");
-console.log(h1Element.innerText);
-h1Element.innerText = "Hello, Tailwind Elements!";
-console.log(document);
-h1Element.style.color = "red";
+let text = document.getElementById("text");
+console.log(text);
+console.log(text.innerText);
+console.log(text.innerHTML);
+
+text.style.color = "red";
+text.style.fontSize = "30px";
+text.style.fontWeight = "bold";
+text.style.textAlign = "center";
+
+text.innerText = "Hello World";
+text.innerHTML = "Hello <b>World</b>";
+
+let image=document.getElementById('img').src= "../img/img.jpg";
+document.getElementById('img').style.width="500px";
+console.log(image);
+console.log(document.getElementById('img'));
+document.getElementById('img').style.display = "none";
+text.style.display = "none";
+
