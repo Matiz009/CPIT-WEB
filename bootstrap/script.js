87abffdd3f198f043c7965eed55f0629a0320a71
Mati ul Rehman <mati.rehman.dev@gmail.com>
2026-02-25 19:08:34 +0500

    js started

---
+++ bootstrap/script.js
@@ -0,0 +1,5 @@
+let h1Element = document.getElementById("h1");
+console.log(h1Element.innerText);
+h1Element.innerText = "Hello, Tailwind Elements!";
+console.log(document);
+h1Element.style.color = "red";
