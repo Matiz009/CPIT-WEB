4c6d5b2b7e52a17b8715dcc05d3fada7f8e84351
Mati ul Rehman <mati.rehman.dev@gmail.com>
2026-02-27 20:37:51 +0500

    html basics done + js query selector work completed

--- bootstrap/script.js
+++ bootstrap/script.js
@@ -1,20 +1,43 @@
-let text = document.getElementById("text");
-console.log(text);
-console.log(text.innerText);
-console.log(text.innerHTML);
+// let text = document.getElementById("text");
+// console.log(text);
+// console.log(text.innerText);
+// console.log(text.innerHTML);
 
-text.style.color = "red";
-text.style.fontSize = "30px";
-text.style.fontWeight = "bold";
-text.style.textAlign = "center";
+// text.style.color = "red";
+// text.style.fontSize = "30px";
+// text.style.fontWeight = "bold";
+// text.style.textAlign = "center";
 
-text.innerText = "Hello World";
-text.innerHTML = "Hello <b>World</b>";
+// text.innerText = "Hello World";
+// text.innerHTML = "Hello <b>World</b>";
 
-let image=document.getElementById('img').src= "../img/img.jpg";
-document.getElementById('img').style.width="500px";
-console.log(image);
-console.log(document.getElementById('img'));
-document.getElementById('img').style.display = "none";
-text.style.display = "none";
+// let image=document.getElementById('img').src= "../img/img.jpg";
+// document.getElementById('img').style.width="500px";
+// console.log(image);
+// console.log(document.getElementById('img'));
+// document.getElementById('img').style.display = "none";
+// text.style.display = "none";
+
+var a=23;
+var a='Hello World';
+var a=true;
+
+let temperature=23;
+temperature=30;
+// let b=45;
+const c=67;
+
+// d=34;
+myFunction();
+function myFunction(){
+    var d=23;
+    console.log('I am in function: '+d);
+}
+console.log('I am out of a function: '+d);
+console.log(a);
+console.log(temperature);
+
+// console.log(b);
+// console.log(c);
+// console.log(d);
 
