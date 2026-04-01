c5e059fc9b89adf57d2b32fc01d48c9e39776df2
Mati ul Rehman <mati.rehman.dev@gmail.com>
2026-04-01 20:24:59 +0500

    promise added

--- async/index.js
+++ async/index.js
@@ -34,3 +34,137 @@
 //     });
 // });
 
+// function sendRequestToJazzcash() {
+//     return new Promise((resolve) => {
+//         setTimeout(() => {
+//             console.log("Request sent to Jazzcash");
+//             resolve();
+//         }, 3000);
+//     });
+// }
+
+// function checkUserBalance() {
+//     return new Promise((resolve) => {
+//         setTimeout(() => {
+//             console.log("Checking user balance...");
+//             resolve();
+//         }, 2000);
+//     });
+// }
+
+// function processPayment() {
+//     return new Promise((resolve) => {
+//         setTimeout(() => {
+//             console.log("Processing payment...");
+//             resolve();
+//         }, 1000);
+//     });
+// }
+
+// function processPaymentSuccess() {
+//     return new Promise((resolve) => {
+//         setTimeout(() => {
+//             console.log("Payment processed successfully!");
+//             resolve();
+//         }, 1000);
+//     });
+// }
+
+// // Using Promises (chaining)
+// sendRequestToJazzcash()
+//     .then(() => checkUserBalance())
+//     .then(() => processPayment())
+//     .then(() => processPaymentSuccess())
+//     .catch((error) => {
+//         console.error("Something went wrong:", error);
+//     });
+
+
+
+
+// using promise
+
+// function sendRequestToJazzcash() {
+//     return new Promise((resolve,reject) => {
+
+//         setTimeout((err) => {
+//             if(err){
+//                 reject("Error sending request to Jazzcash");
+//             }
+//             fetch("https://jsonplaceholder.typicode.com/posts/");
+//             console.log("Data fetched from API...");
+//             resolve();
+//         }, 3000);
+//     });
+// }
+
+// function checkUserBalance() {
+//     return new Promise((resolve,reject) => {
+//         setTimeout((err) => {
+//             if(err){
+//                 reject("Error checking user balance");
+//             }
+//             console.log("Checking user balance...");
+//             fetch("https://jsonplaceholder.typicode.com/users/");
+//             console.log("User data fetched from API...");
+//             resolve();
+//         }, 2000);
+//     });
+// }
+
+// function processPayment() {
+//     return new Promise((resolve,reject) => {
+//         setTimeout((err) => {
+//             if(err){
+//                 reject("Error processing payment");
+//             }
+//             console.log("Processed payment...");
+//             resolve();
+//         }, 3000);
+//     });
+// }
+
+
+// sendRequestToJazzcash()
+//     .then(() => checkUserBalance())
+//     .then(() => processPayment())
+//     .catch((error) => {
+//         console.error("Something went wrong:", error);
+//     }).finally(() => {
+//         console.log("Payment process completed.");
+//     });
+
+
+
+// using async await
+
+async function sendRequestToJazzcash() {
+ console.log("Request sent to Jazzcash");
+}
+
+async function checkUserBalance() {
+console.log("Checking user balance...");
+}
+
+async function processPayment() {
+console.log("Processing payment...");
+}
+
+async function processPaymentSuccess() {
+console.log("Payment processed successfully!");
+}
+
+async function main() {
+    try {
+        await sendRequestToJazzcash();
+        await checkUserBalance();
+        await processPayment();
+        await processPaymentSuccess();
+    } catch (error) {
+        console.error("Something went wrong:", error);
+    } finally {
+        console.log("Payment process completed.");
+    }
+}
+
+main();
