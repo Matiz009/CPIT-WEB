70dd41e0c5ea2977ae03512d4ee468b1914094e1
Mati ul Rehman <mati.rehman.dev@gmail.com>
2026-04-01 19:23:29 +0500

    callbacks added

---
+++ async/index.js
@@ -0,0 +1,36 @@
+// function sendRequestToJazzcash(callback) {
+//     setTimeout(() => {
+//         console.log("Request sent to Jazzcash");
+//         callback();
+//     }, 3000);
+// }
+
+// function checkUserBalance(callback) {
+//     setTimeout(() => {
+//         console.log("Checking user balance...");
+//         callback();
+//     }, 2000);
+// }
+
+// function processPayment(callback) {
+//     setTimeout(() => {
+//         console.log("Processing payment...");
+//         callback();
+//     }, 1000);
+// }
+
+// function processPaymentSuccess() {
+//     setTimeout(() => {
+//     console.log("Payment processed successfully!");
+//     }, 1000);
+// }
+
+// // Using callback
+// sendRequestToJazzcash(() => {
+//     checkUserBalance(() => {
+//         processPayment(() => {
+//             processPaymentSuccess();
+//         });
+//     });
+// });
+
---
+++ functions/functions.js
@@ -0,0 +1,20 @@
+// window.console.log("functions.js loaded");
+
+// navigator.geolocation.getCurrentPosition(function(position) {
+//    try {
+//      console.log("Latitude: " + position.coords.latitude);
+//      console.log("Longitude: " + position.coords.longitude);
+//    } catch (error) {
+//     console.log(error);
+//    }
+// });
+
+// //local storage
+
+// localStorage.setItem("name", "John Doe");
+// var name = localStorage.getItem("name");
+// console.log("Name from local storage: " + name);
+
+
+
+// Problem with regular function
