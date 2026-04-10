06d165181c7da9a083de22092f6050ec2ec3fb0b
Mati ul Rehman <mati.rehman.dev@gmail.com>
2026-04-10 09:26:02 +0500

    added a task

--- node/index.js
+++ node/index.js
@@ -43,25 +43,41 @@
 
 // // http module
 
-const PORT=3000;
+const PORT = 3000;
+const fs = require('fs');
 const http = require('http');
-http.createServer((req,res)=>{
-    // res.writeHead(200,{'Content-Type':'text/html'});
-    // res.write('<div><h1>Hello World</h1><br/><p>This is a simple Node.js server.</p></div>');
+
+http.createServer((req, res) => {
+
     if(req.url === '/'){
         res.writeHead(200,{'Content-Type':'text/html'});
-        res.write('<div><h1>Home Page</h1><br/><p>Welcome to the home page.</p></div>');
-    }
-    if(req.url === '/products'){
+        res.end('<div><h1>Home Page</h1><p>Welcome to the home page.</p></div>');
+    } 
+    else if(req.url === '/products'){
         const products = [
             {id:1,name:'Product 1',price:10},
             {id:2,name:'Product 2',price:20},
             {id:3,name:'Product 3',price:30}
         ];
         res.writeHead(200,{'Content-Type':'application/json'});
-        res.write(JSON.stringify(products));
-    }     
-    res.end();
-}).listen(PORT,()=>{
+        res.end(JSON.stringify(products));
+    } 
+    else if(req.url === '/users'){
+        fs.readFile('users.json', (error, data) => {
+            if(error){
+                res.writeHead(500,{'Content-Type':'application/json'});
+                res.end(JSON.stringify({error: 'Failed to read users file'}));
+            } else {
+                res.writeHead(200,{'Content-Type':'application/json'});
+                res.end(data);
+            }
+        });
+    } 
+    else {
+        res.writeHead(404, {'Content-Type':'text/plain'});
+        res.end('404 Not Found');
+    }
+
+}).listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
